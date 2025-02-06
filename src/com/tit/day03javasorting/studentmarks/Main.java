eef680036d5386f05f397f6a62d8ba7fa619a011
Karan3872 <karananuragi7879@gmail.com>
2025-02-06 12:30:37 +0530

    [Karan Anuragi]Add.Program01 of Sorting in which we have sort student marks in bubble sort

---
+++ src/com/tit/day03javasorting/studentmarks/Main.java
@@ -0,0 +1,22 @@
+package com.tit.day03javasorting.studentmarks;
+
+// Creating a class SortStudentMarks to demonstrate the Bubble Sort algorithm
+class Main
+{
+    public static void main(String[] args) {
+        // Array of student marks
+        int[] marks = {87, 76, 90, 65, 78, 92, 80, 64, 33, 46, 88 , 44};
+
+        // Create an instance of BubbleSort
+        BubbleSort sorter = new BubbleSort();
+
+        System.out.println("Original Marks:");
+        sorter.printArray(marks);
+
+        // Perform Bubble Sort
+        sorter.bubbleSort(marks);
+
+        System.out.println("Sorted Marks:");
+        sorter.printArray(marks);
+    }
+}
