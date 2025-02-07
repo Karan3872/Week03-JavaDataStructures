558e745490b73fd871b78c81661610e709d71150
Karan3872 <karananuragi7879@gmail.com>
2025-02-07 20:57:52 +0530

    [Karan Anuragi]Add.Program11 of BinarySearch in which we have to searching rotation point in rotated sorted array in BinarySearch

---
+++ src/main/java/com/tit/day0405searchingreadingstringtype/binarysearch/rotationpoint/Main.java
@@ -0,0 +1,13 @@
+package com.tit.day0405searchingreadingstringtype.binarysearch.rotationpoint;
+
+public class Main
+{
+    public static void main(String[] args) {
+        // Example rotated sorted array for testing
+        int[] array = {6, 7, 9,13, 15, 19, 2, 3,4};
+
+        // Find and print the index of the rotation point
+        int rotationPointIndex = BinarySearch.findRotationPoint(array);
+        System.out.println("The rotation point is at index: " + rotationPointIndex);
+    }
+}
