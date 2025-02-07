c82fb9242662444844aeef38a1d37c5112832826
Karan3872 <karananuragi7879@gmail.com>
2025-02-07 20:40:33 +0530

    [Karan Anuragi]Add.Program10 of LinearSearch in which we have to finding first negative element of array in LinearSearch

---
+++ src/main/java/com/tit/day0405searchingreadingstringtype/linearsearch/firstnegativenumber/Main.java
@@ -0,0 +1,17 @@
+package com.tit.day0405searchingreadingstringtype.linearsearch.firstnegativenumber;
+
+public class Main
+{
+    public static void main(String[] args) {
+        // Example array for testing
+        int[] array = {3, 5, 7, -2, 8, -6, 4};
+
+        // Find and print the index of the first negative number
+        int index = LinearSearch.findFirstNegative(array);
+        if (index != -1) {
+            System.out.println("The first negative number is at index: " + index);
+        } else {
+            System.out.println("No negative number found in the array.");
+        }
+    }
+}
